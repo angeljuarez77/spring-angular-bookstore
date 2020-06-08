2a3479e72a6d4a2cdec7c4d2ac37f3558232aaef
Angel Juarez <angeljuarez729@gmail.com>
2020-06-07 22:00:12 -0400

    all users route

--- server/src/main/java/com/example/server/controller/UserController.java
+++ server/src/main/java/com/example/server/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.example.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.server.models.User;
+import com.example.server.service.UserService;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
+
     @GetMapping
-    public String users() {
-        return "users";
+    public Iterable<User> allUsers() {
+        return userService.listUsers();
     }
 }
