5c69dd409adf550327b8c5431849522cb7f53cfd
Angel Juarez <angeljuarez729@gmail.com>
2020-06-08 16:44:15 -0400

    delete user

--- server/src/main/java/com/example/server/controller/UserController.java
+++ server/src/main/java/com/example/server/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.server.models.User;
@@ -18,7 +19,18 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public User signup(@RequestBody User user) {
+    public User signUp(@RequestBody User user) {
         return userService.createUser(user);
     }
+
+    @PostMapping("/login")
+    public User logIn(@RequestBody User user) {
+        return userService.logIn(user.getUsername(), user.getPassword());
+    }
+
+    @DeleteMapping("/{userid}")
+    public HttpStatus deleteUser(@PathVariable Long userid) {
+        return userService.deleteById(userid);
+    }
+
 }
