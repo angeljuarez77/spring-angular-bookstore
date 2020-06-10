e4e6e37dbf3cb3d48db5c9b4c562f675c55f89b6
Angel Juarez <angeljuarez729@gmail.com>
2020-06-10 14:52:26 -0400

    service for http calls
    getUsers() function functional
    
    signUp() function has the issues with CORS

--- client/src/app/user.service.ts
+++ client/src/app/user.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { User } from "../user"; // hopefully going to be used later
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { User } from "./user"; // hopefully going to be used later
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private userUrl = "http://localhost:8181/user"
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+  }
 
   constructor(private http: HttpClient) { }
 
   getUsers() {
     return this.http.get(this.userUrl);
+  }
+
+  signUp(user: User): Observable<User> {
+    return this.http.post<User>(this.userUrl, user, this.httpOptions);
   }
 }
