f330a44a8d4089dc401390089aaca66023f6d599
Angel Juarez <angeljuarez729@gmail.com>
2020-06-10 14:35:40 -0400

    Beginning entity class
    Will not be used until cors issues are resolved

---
+++ server/src/main/java/com/example/server/models/UserProfile.java
@@ -0,0 +1,12 @@
+// package com.example.server.models;
+//
+// import javax.persistence.*;
+//
+// @Entity
+// @Table(name = "user_profile")
+// public class UserProfile {
+//     @Id
+//     @Column
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     Long profileId;
+// }
