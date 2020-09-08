9b5514ec66caa2f986eba832352da2fb9e7f8bc2
anonymoususerdemo <angelsmedia77@gmail.com>
2020-09-08 14:16:11 -0400

    test

--- server/src/main/java/com/example/server/config/DatabaseConfig.java
+++ server/src/main/java/com/example/server/config/DatabaseConfig.java
@@ -4,11 +4,12 @@ import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
-
+// testing
 @Configuration
 public class DatabaseConfig {
     @Value("${spring.datasource.url}")
     private String dbUrl;
+
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
